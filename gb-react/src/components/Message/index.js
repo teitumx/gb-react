a3201b63e3ae3a9e49d2af03f8e7e8daf3dcaf38
teitumx <kirik241@ya.ru>
2021-11-23 11:16:31 +0300

    homework #3

--- gb-react/src/components/Message/index.js
+++ gb-react/src/components/Message/index.js
@@ -2,15 +2,11 @@ import React from "react";
 
 function Message({ author, message, i }) {
   return (
-    <li key={i} className="clearfix">
-      <div className="message-data text-right">
-        <span className="name-sent">{author}</span>
-        <img
-          src="https://bootdey.com/img/Content/avatar/avatar7.png"
-          alt="avatar"
-        />
+    <li key={i} className="messages">
+      <div className="messageBox">
+        <div className="name-sent">{author}</div>
+        <div className="message other-message float-right">{message}</div>
       </div>
-      <div className="message other-message float-right">{message}</div>
     </li>
   );
 }
